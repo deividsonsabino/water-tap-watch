80b2961f845e2c014ec810ad5b24b064715c3a23
Deivao <xdeividsabinox@live.com>
2025-10-25 17:04:54 -0300

    Initial Commit

---
+++ WaterTap Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WaterTap Watch App
+//
+//  Created by Deividson Sabino on 25/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaterTap Watch App/WaterTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTapApp.swift
+//  WaterTap Watch App
+//
+//  Created by Deividson Sabino on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterTap_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
