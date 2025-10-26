c1eb7d1f3e711ec7c5db46599825410f1065d00b
Deivao <xdeividsabinox@live.com>
2025-10-26 11:21:16 -0300

    feat(ui): componentize iOS views and add multiple previews for UI/UX testing

---
+++ WaterTap iOS/App/WaterTap_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTap_iOSApp.swift
+//  WaterTap iOS
+//
+//  Created by Deividson Sabino on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterTap_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
